26818567af086d53f612dbab49d20af84cabe9a6
Annie Cai <caishajia@gmail.com>
2023-01-23 11:22:03 -0500

    sql query

--- backend (spring)/src/main/java/com/aanxniee/urlshortener/repository/URLRepository.java
+++ backend (spring)/src/main/java/com/aanxniee/urlshortener/repository/URLRepository.java
@@ -2,6 +2,9 @@ package com.aanxniee.urlshortener.repository;
 
 import com.aanxniee.urlshortener.modal.URL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface URLRepository extends JpaRepository<URL, Integer> {
+    @Query(value = "select originalUrl from URL where shortUrl = ?1", nativeQuery = true)
+    String findByShortUrl(String id);
 }
