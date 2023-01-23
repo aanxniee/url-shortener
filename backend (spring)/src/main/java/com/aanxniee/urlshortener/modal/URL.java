eeb35a5011684038fd9b214b7571bf889e497ff9
Annie Cai <caishajia@gmail.com>
2023-01-23 11:12:03 -0500

    created url class

--- backend (spring)/src/main/java/com/aanxniee/urlshortener/modal/URL.java
+++ backend (spring)/src/main/java/com/aanxniee/urlshortener/modal/URL.java
@@ -1,4 +1,21 @@
 package com.aanxniee.urlshortener.modal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity // defines class URL as a table in the SQL database
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class URL {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String shortURL;
+    private  String originalURL;
 }
