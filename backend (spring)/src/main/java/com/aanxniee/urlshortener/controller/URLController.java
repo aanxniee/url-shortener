5a0bb538200baf604a17e720a640b35f8dec16f6
Annie Cai <caishajia@gmail.com>
2023-01-23 11:20:47 -0500

    created rest calls

--- backend (spring)/src/main/java/com/aanxniee/urlshortener/controller/URLController.java
+++ backend (spring)/src/main/java/com/aanxniee/urlshortener/controller/URLController.java
@@ -1,4 +1,25 @@
 package com.aanxniee.urlshortener.controller;
 
+import com.aanxniee.urlshortener.modal.URL;
+import com.aanxniee.urlshortener.service.URLService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController // marks class as a request handler, creates RESTful API calls
+@RequestMapping // maps requests onto specific handlers
 public class URLController {
+    @Autowired
+    private URLService urlService;
+
+    @GetMapping("/{id")
+    // handles HTTPs GET requests, retrieves URL by specified id
+    public String getOriginalUrl(@PathVariable String id) {
+        return urlService.getOriginalUrl(id);
+    }
+
+    @PostMapping
+    // handles HTTPs POST requests, creates short URL for a given URL
+    public URL generateShortUrl(@RequestBody String url) {
+        return urlService.generateShortUrl(url);
+    }
 }
--- backend (spring)/src/main/java/com/aanxniee/urlshortener/service/URLService.java
+++ backend (spring)/src/main/java/com/aanxniee/urlshortener/service/URLService.java
@@ -1,5 +1,6 @@
 package com.aanxniee.urlshortener.service;
 
+import com.aanxniee.urlshortener.modal.URL;
 import com.aanxniee.urlshortener.repository.URLRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,11 @@ import org.springframework.stereotype.Service;
 public class URLService {
     @Autowired
     private URLRepository urlRepository;
+
+    public String getOriginalUrl(String id) {
+        return urlRepository.findByShortUrl(id);
+    }
+    public URL generateShortUrl(String url) {
+
+    }
 }
