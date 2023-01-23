5c730c9717663e222f6ead5e8577a5284c24e200
Annie Cai <caishajia@gmail.com>
2023-01-23 11:31:43 -0500

    class to hash a url and check if its valid

---
+++ backend (spring)/src/main/java/com/aanxniee/urlshortener/logic/GenerateShortUrl.java
@@ -0,0 +1,17 @@
+package com.aanxniee.urlshortener.logic;
+
+import com.google.common.hash.Hashing;
+import org.apache.commons.validator.routines.UrlValidator;
+
+import java.nio.charset.Charset;
+
+public class GenerateShortUrl {
+    public static String getShortUrl(String url) {
+        String shortUrl = Hashing.murmur3_32_fixed().hashString(url, Charset.defaultCharset()).toString();
+        return shortUrl;
+    }
+    public static boolean isUrlValid(String url) {
+        boolean res = UrlValidator.getInstance().isValid(url);
+        return res;
+    }
+}
